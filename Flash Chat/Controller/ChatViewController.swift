b7955b0714cc508e3da07c1f6edf6e17003ea1a1
Serik Abdirov <smabdirov99@icloud.com>
2022-06-20 14:51:35 +0300

    add firebase listener

--- Flash Chat/Controller/ChatViewController.swift
+++ Flash Chat/Controller/ChatViewController.swift
@@ -22,6 +22,7 @@ class ChatViewController: UIViewController {
         super.viewDidLoad()
         tableView.dataSource = self
         navigationItem.hidesBackButton = true
+        title = Constants.appName
 
         tableView.register(UINib(nibName: Constants.cellNibName, bundle: nil), forCellReuseIdentifier: Constants.cellIdentifier)
 
@@ -30,8 +31,11 @@ class ChatViewController: UIViewController {
     }
 
     func loadMessages() {
-        messages = []
-        db.collection(Constants.FStore.collectionName).getDocuments { querySnapshot, error in
+
+        db.collection(Constants.FStore.collectionName)
+            .order(by: Constants.FStore.dateField)
+            .addSnapshotListener { querySnapshot, error in
+            self.messages = []
             if let e = error {
                 let alert = UIAlertController(title: "Error", message: e.localizedDescription, preferredStyle: .alert)
                 alert.addAction(UIAlertAction(title: "Ok", style: .default))
@@ -56,21 +60,25 @@ class ChatViewController: UIViewController {
     }
 
     @IBAction func sendPressed(_ sender: UIButton) {
-
-        if let messageBody = messageTextfield.text, let messageSender = Auth.auth().currentUser?.email {
-            db.collection(Constants.FStore.collectionName).addDocument(data: [
-                Constants.FStore.senderField: messageSender,
-                Constants.FStore.bodyField: messageBody
-            ]) { error in
-                if let e = error {
-                    let alert = UIAlertController(title: "Error", message: e.localizedDescription, preferredStyle: .alert)
-                    alert.addAction(UIAlertAction(title: "Ok", style: .default))
-                    self.present(alert, animated: true)
-                } else {
-                    print("success")
+        if messageTextfield.text != "" {
+            if let messageBody = messageTextfield.text, let messageSender = Auth.auth().currentUser?.email {
+                db.collection(Constants.FStore.collectionName).addDocument(data: [
+                    Constants.FStore.senderField: messageSender,
+                    Constants.FStore.bodyField: messageBody,
+                    Constants.FStore.dateField: Date().timeIntervalSince1970
+                ]) { error in
+                    if let e = error {
+                        let alert = UIAlertController(title: "Error", message: e.localizedDescription, preferredStyle: .alert)
+                        alert.addAction(UIAlertAction(title: "Ok", style: .default))
+                        self.present(alert, animated: true)
+                    } else {
+                        print("success")
+                    }
                 }
             }
         }
+
+        messageTextfield.text = ""
 
     }
 
